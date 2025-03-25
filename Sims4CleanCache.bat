bc2547c6a8db29cc353637f3b9e0068ef93915ec
Brendan <98478167+ETES-BrendanReed@users.noreply.github.com>
2025-03-24 22:16:11 -0400

    Update [1]

--- Sims4CleanCache.bat
+++ Sims4CleanCache.bat
@@ -1,26 +1,27 @@
 @ECHO OFF
 SETLOCAL ENABLEDELAYEDEXPANSION
-
 SET "localthumbcache=C:\Users\%Username%\Documents\Electronic Arts\The Sims 4\localthumbcache.package"
 SET "avatarcache=C:\Users\%Username%\Documents\Electronic Arts\The Sims 4\avatarcache.package"
 SET "lastexception=C:\Users\%Username%\Documents\Electronic Arts\The Sims 4\lastexception*.txt"
 SET "lastcrash=C:\Users\%Username%\Documents\Electronic Arts\The Sims 4\lastcrash*.txt"
 SET "notifyglob=C:\Users\%Username%\Documents\Electronic Arts\The Sims 4\notify.glob"
-SET screenshots="C:\Users\%Username%\Documents\Electronic Arts\The Sims 4\screenshots\*.png"
+SET "sims4folder=C:\Users\%Username%\Documents\Electronic Arts\The Sims 4\"
 
-REM Functions to check and delete files
-CALL :CheckAndDelete "%localthumbcache%"
-CALL :CheckAndDelete "%avatarcache%"
-CALL :CheckAndDelete "%lastexception%"
-CALL :CheckAndDelete "%lastcrash%"
-CALL :CheckAndDelete "%notifyglob%"
-FOR %%F IN (%screenshots%) DO CALL :CheckAndDeleteY/N "%%F"
+@REM Functions to check and delete files
+CALL :DeleteFiles "%localthumbcache%"
+CALL :DeleteFiles "%avatarcache%"
+CALL :DeleteFiles "%lastexception%"
+CALL :DeleteFiles "%lastcrash%"
+CALL :DeleteFiles "%notifyglob%"
+FOR %%F IN ("%sims4folder%\screenshots\*.png") DO CALL :CheckAndDeleteY/N "%%F" ELSE (
+    ECHO No more screenshots found.
+)
 
-ECHO Waiting for 5 seconds...
+ECHO Exiting in 5 seconds...
 TIMEOUT /T 5 /NOBREAK
 EXIT
 
-:CheckAndDelete
+:DeleteFiles
 IF EXIST "%~1" (
     SET "filename=%~nx1"
     DEL "%~1"
@@ -28,51 +29,42 @@ IF EXIST "%~1" (
 ) ELSE (
     ECHO %~1 does not exist.
 )
-EXIT /B
 
 :CheckAndDeleteY/N
 IF EXIST "%~1" (
     SET "filename=%~nx1"
-    SET /P "action=(D=Delete all screenshots) or (C=Check each file)? "
+    SET /P "action=Screenshots - Delete all (D) or Check each file (C): "
     IF /I "!action!"=="D" (
-        FOR %%F IN (%screenshots%) DO (
-            DEL "%%F"
-            ECHO %%F deleted.
-        )
-        EXIT /B
+        DEL "%sims4folder%\screenshots\*.png"
+        ECHO All Screenshots deleted.
+        ECHO Exiting in 5 seconds...
+        TIMEOUT /T 5 /NOBREAK
+        EXIT
     ) ELSE IF /I "!action!"=="C" (
-        GOTO CheckEachFile
+        GOTO :CheckEachFile
     ) ELSE (
-        ECHO Invalid choice. Exiting.
-        EXIT /B
+        ECHO Invalid choice. Select again.
+        GOTO :CheckAndDeleteY/N
     )
 )
-
 :CheckEachFile
-SET "lastFile="
-FOR %%F IN (%screenshots%) DO (
-    SET "lastFile=%%F"
-)
-
-FOR %%F IN (%screenshots%) DO (
-    IF EXIST "%%F" (
-        SET "filename=%%~nxF"
-        SET /P "choice=!filename! | (D=Delete/S=Skip/V=View): "
-        IF /I "!choice!"=="D" (
-            DEL "%%F"
-            ECHO !filename! deleted.
-        ) ELSE IF /I "!choice!"=="S" (
-            ECHO !filename! skipped.
-            IF "%%F"=="!lastFile!" (
-                EXIT
-            )
-        ) ELSE IF /I "!choice!"=="V" (
-            START "" "%%F"
-            ECHO Viewing !filename!. Close the file to continue.
-            PAUSE
-        ) ELSE (
-            ECHO Invalid choice. Please enter D, S, or V.
-        )
+    IF EXIST "%~1" (
+        SET "filename=%~nx1"
+        SET /P "choice= !filename! - (D=Delete/S=Skip/V=View): "
+    IF /I "!choice!"=="D" (
+        DEL "%~1"
+        ECHO !filename! deleted.
+        GOTO :CheckEachFile
+    ) ELSE IF /I "!choice!"=="S" (
+        ECHO !filename! skipped.
+    ) ELSE IF /I "!choice!"=="V" (
+        START "" "%~1"
+        ECHO Viewing !filename!. Close the file to continue.
+        PAUSE
+        GOTO :CheckEachFile
+    ) ELSE (
+        ECHO Invalid choice. Please enter D, S, or V.
+        GOTO :CheckEachFile
     )
 )
 EXIT /B
